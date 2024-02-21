74532f0e9bc5e77aa276a6d3c3cd30c8f4ca2783
kawaiicakes <vomitmopper@gmail.com>
2024-02-21 16:55:03 -0700

    package info

---
+++ src/main/java/io/github/kawaiicakes/sleepingaround/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package io.github.kawaiicakes.sleepingaround;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
